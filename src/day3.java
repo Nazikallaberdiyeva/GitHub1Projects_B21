0a0c8c95a0a2a1d2efe3ca35b71111525c754472
Nazikallaberdiyeva <nazikallaberdiyeva@gmail.com>
2020-11-12 09:57:16 -0600

    initial commit

---
+++ src/day3.java
@@ -0,0 +1,13 @@
+public class day3 {
+    public static void main(String[] args) {
+        // write your code here//////////////
+        System.out.println("Hello Git Last Day");
+        // b 21 is excited.
+        // mklwdsmvopld'
+
+// final work is done
+//kjklml;l
+// kslkl;l
+
+    }
+}
